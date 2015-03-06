9ef77a718e421e92fd3e1618dc4d3160fef3f684
baranmcl <baranmcl@gmail.com>
2015-03-06 13:19:25 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ apt-get install python-pandas
 chmod a+x runningmedian.py
 chmod a+x wordcount.py
 
-python wordcount.py ./wc_input ./wc_output/wc_result.txt
+python wordcount.py ./wc_input ./wc_output/wc_result.txt &
 python runningmedian.py ./wc_input ./wc_output/rm_result.txt
