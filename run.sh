5da2bf0dbef316ec61c7f2ced8f0fa42b75b6bf5
baranmcl <baranmcl@gmail.com>
2015-03-06 12:06:42 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+
+apt-get install python-pandas
+
+chmod a+x runningmedian.py
+chmod a+x wordcount.py
+
+python wordcount.py ./wc_input ./wc_output/wc_result.txt
+python runningmedian.py ./wc_input ./wc_output/rm_result.txt
