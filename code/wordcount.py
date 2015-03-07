c16a6398a3fa9409022a26fe25b5b44004255ec4
baranmcl <baranmcl@gmail.com>
2015-03-07 13:28:42 -0500

    Update wordcount.py

--- code/wordcount.py
+++ code/wordcount.py
@@ -1,24 +1,25 @@
 #WordCount
 import os
 
-path1 = 'InsightData/wc_input'
-path2 = 'InsightData/wc_output'
-listing = os.listdir(path1)
+if __name__ == '__main__':
+    path1 = 'InsightData/wc_input' #set input and output paths
+    path2 = 'InsightData/wc_output'
+    listing = os.listdir(path1)
 
-words = {}
+    words = {} #initialize words dictionary
 
-os.chdir(path2)
-writefile = open("wc_result.txt", "w")
+    os.chdir(path2) #create result file
+    writefile = open("wc_result.txt", "w")
 
-for infile in listing:
+for infile in listing: #loop through input files
     os.chdir(path1)
     readfile = open("%s" %(infile), "r")
-    for word in readfile.read().split():
+    for word in readfile.read().split(): #loop through each word in input files
         if word not in words:
             words[word] = 1
         if word in words:
             words[word] = words[word] + 1
-    for key in words.keys():
+    for key in words.keys(): #write words and values to output
         os.chdir(path2)
         writefile.write("%s\t\t%s\n" %(key , words[key]))
 
