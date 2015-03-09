4089cfc399c6312bbbc89aba77655b2702972cf4
baranmcl <baranmcl@gmail.com>
2015-03-08 21:41:44 -0400

    Update runningmedian.py

--- code/runningmedian.py
+++ code/runningmedian.py
@@ -1,5 +1,6 @@
 #Running Median
 import os
+import heapq
 
 if __name__ == '__main__':
     path1 = 'InsightData/input'
@@ -7,23 +8,34 @@ if __name__ == '__main__':
     listing = os.listdir(path1)
     
     linesprocessed = 0 #initialize lines processed counter
-    MaxHeap = [] #initialize heaps
-    MinHeap = []
-    MaxLength = len(MaxHeap)
-    MinLength = len(MinHeap)
+    MaxHeap = [] #every key <= to current median
+    MinHeap = [] #every key >= to current median
+ 
     
     os.chdir(path2) #write output file in correct location
     writefile = open("med_result.txt", "w")
 
-def findmedian(x, y):
-    if (len(x)+len(y)) % 2 == 0:
-        median = (min(x) + max(y)) / 2.0 #median is average of two middle-most numbers
-        return median
-    elif (len(x)+len(y)) % 2 == 1: #if the number of lines is odd, median is in the middle
-        if len(x) > len(y):
-            return min(x)
-        elif len(x) < len(y):
-            return max(y)
+def RebalanceHeap():
+    if (len(MaxHeap) - len(MinHeap)) > 1:
+        heapq.heappush(MinHeap, max(MaxHeap))
+        max(MaxHeap) = MaxHeap[-1]
+        MaxHeap.pop()
+        heapify(MaxHeap)
+    elif (len(MinHeap) - len(MaxHeap)) > 1:
+        heapq.heappush(MaxHeap, min(MinHeap)))
+        min(MinHeap) = MinHeap[-1]
+        MinHeap.pop()
+        heapq.heapify(MinHeap)
+
+def findmedian():
+    if len(MaxHeap) + len(MinHeap) == 0:
+        return 0
+    elif len(MinHeap) > len(MaxHeap):
+        return MinHeap[0]
+    elif len(MaxHeap) > len(MinHeap):
+        return MaxHeap[0]
+    elif len(MinHeap) == len(MaxHeap)
+        return ((MaxHeap[0] + MinHeap[0])/ 2.0)
 
 
 for infile in listing: #loop through each input file
@@ -31,45 +43,27 @@ for infile in listing: #loop through each input file
     readfile = open("%s" %(infile), "r")
     
     for line in iter(readfile): #loop through each line in input file
-        line = line.strip().split()
-        
-        if linesprocessed >= 2: #rebalance heaps
-            if (MaxLength - MinLength) > 1:
-                MinHeap.append(max(MaxHeap))
-                MaxHeap.remove(max(MaxHeap))
-            elif (MinLength - MaxLength) > 1:
-                MaxHeap.append(min(MinHeap))
-                MinHeap.remove(min(MinHeap))
-
-        if linesprocessed == 0: #calculate current median
-            currentmedian = 0
-        elif linesprocessed == 1:
-            currentmedian = MaxHeap[0]
-        else:
-            currentmedian = findmedian(MaxHeap, MinHeap) #calculate current running median
-
-        if len(line) > currentmedian: #place line word count in appropriate heap
-            MaxHeap.append(len(line))
-        elif len(line) < currentmedian:
-            MinHeap.append(len(line))
-        elif len(line) == currentmedian:
-            if MaxLength > MinLength:
-                MinHeap.append(len(line))
-            else:
-                MaxHeap.append(len(line))
-        linesprocessed = linesprocessed + 1
+        line = line.strip().replace("-", " ").split(" ")
+            currentmedian = findmedian()
+            if len(line) < currentmedian:
+                heapq.heappush(MaxHeap, len(line))
+                RebalanceHeap()
+            elif len(line) > currentmedian:
+                heapq.heappush(MinHeap, len(line))
+                RebalanceHeap()
+            elif len(line) == currentmedian:
+                if len(MaxHeap) > len(MinHeap):
+                    heapq.heappush(MinHeap, len(line))
+                    RebalanceHeap()
+                elif len(MaxHeap) < len(MinHeap):
+                    heapq.heappush(MaxHeap, len(line))
+                    RebalanceHeap()
+                elif len(MaxHeap) == len(MinHeap):
+                    heapq.heappush(MaxHeap, len(line))
+                    RebalanceHeap()
     readfile.close()
-    
-if linesprocessed >= 2: #rebalance heaps for final median calculation
-    if (MaxLength - MinLength) > 1:
-        MinHeap.append(max(MaxHeap))
-        MaxHeap.remove(max(MaxHeap))
-    elif (MaxLength - MinLength) > 1:
-        MaxHeap.append(min(MinHeap))
-        MinHeap.remove(min(MinHeap))
 
-currentmedian = findmedian(MaxHeap, MinHeap) #calculate final median
-
-writefile.write("The Running Median Words Per Line is %s!" %(currentmedian))
+currentmedian = findmedian()
+writefile.write("The running median words per line is %s!" %(currentmedian))
 
 writefile.close()
