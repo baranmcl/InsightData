0779a5f43cf53b0dc1e9ce74265b3e1a49f510ea
baranmcl <baranmcl@gmail.com>
2015-03-08 23:36:34 -0400

    Update wordcount.py

--- code/wordcount.py
+++ code/wordcount.py
@@ -1,29 +1,27 @@
 #WordCount
 import os
 
-if __name__ == '__main__':
-    path1 = 'InsightData/wc_input' #set input and output paths
-    path2 = 'InsightData/wc_output'
-    listing = os.listdir(path1)
+path1 = 'InsightData/wc_input'
+path2 = 'InsightData/wc_output'
+listing = os.listdir(path1)
 
-    words = {} #initialize words dictionary
+words = {}
 
-    os.chdir(path2) #create result file
-    writefile = open("wc_result.txt", "w")
+os.chdir(path2)
+writefile = open("wc_result.txt", "w")
 
 for infile in listing: #loop through input files
     os.chdir(path1)
     readfile = open("%s" %(infile), "r")
-    for word in readfile.read().replace("-", " ").split(" "): #loop through each word in input files, replacing hyphens
+    for word in readfile.read().lower().strip().replace("-", " ").split(): #loop through each word in input files, making all words lowercase and replacing hyphens
         if word not in words:
             words[word] = 1
-        if word in words:
+        elif word in words:
             words[word] = words[word] + 1
     readfile.close()
 
-words = sorted(words.keys()) #sort keys of dictionary into alphabetical order
 
 os.chdir(path2)
-for key in words.keys(): #write words and values to output
+for key in sorted(words): #write words and values to output
     writefile.write("%s\t\t%s\n" %(key , words[key]))
 writefile.close()
