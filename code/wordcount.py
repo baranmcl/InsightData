607a991a3fa885fdc9b82d7c2dd52521fdea56dc
baranmcl <baranmcl@gmail.com>
2015-03-09 00:54:34 -0400

    Update wordcount.py

--- code/wordcount.py
+++ code/wordcount.py
@@ -11,21 +11,21 @@ if __name__ == '__main__':
     os.chdir(path2) #write result txt in correct location
     writefile = open("wc_result.txt", "w")
 
-for infile in sorted(listing): #loop through input files
-    os.chdir(path1)
-    readfile = open("%s" %(infile), "r")
-    for word in readfile.read().lower().strip().replace("-", " ").split(): #loop through each word in input files, making all words lowercase and replacing hyphens
-        for char in word: #remove unwanted punctuation
-            if char in '()!,?.;:"/\<>&^%$#@~`':
-                char = ""
-        if word not in words:
-            words[word] = 1
-        elif word in words:
-            words[word] = words[word] + 1
-    readfile.close()
+    for infile in sorted(listing): #loop through input files
+        os.chdir(path1)
+        readfile = open("%s" %(infile), "r")
+        for word in readfile.read().lower().strip().replace("-", " ").split(): #loop through each word in input files, making all words lowercase and replacing hyphens
+            for char in word: #remove unwanted punctuation
+                if char in '()!,?.;:"/\<>&^%$#@~`':
+                    char = ""
+            if word not in words:
+                words[word] = 1
+            elif word in words:
+                words[word] = words[word] + 1
+        readfile.close()
 
 
-os.chdir(path2)
-for key in sorted(words): #write words and values to output
-    writefile.write("%s\t\t%s\n" %(key , words[key]))
-writefile.close()
+    os.chdir(path2)
+    for key in sorted(words): #write words and values to output
+        writefile.write("%s\t\t%s\n" %(key , words[key]))
+    writefile.close()
