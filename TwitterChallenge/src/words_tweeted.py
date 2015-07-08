cf708c8998d268da0716708a6dd64a4269f968bc
baranmcl <baranmcl@gmail.com>
2015-07-08 18:46:34 -0400

    removed looping through multiple input files
    now only opens/reads "tweets.txt"

--- TwitterChallenge/src/words_tweeted.py
+++ TwitterChallenge/src/words_tweeted.py
@@ -8,15 +8,14 @@ listing = os.listdir(path1)
 words = {}
 
 def wordcount(x):
-    for infile in x: #loop through input files
-        os.chdir(path1)
-        readfile = open("%s" %(infile), "r")
-        for word in readfile:
-            if word not in words:
-                words[word] = 1
-            elif word in words:
-                    words[word] = words[word] + 1
-        readfile.close()
+    os.chdir(path1)
+    readfile = open("tweets.txt", "r")
+    for word in readfile:
+        if word not in words:
+            words[word] = 1
+        elif word in words:
+            words[word] = words[word] + 1
+    readfile.close()
 
     os.chdir(path2)
     for key in sorted(words): #write words and values to output
