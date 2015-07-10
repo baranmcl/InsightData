30796c5b69ec33578e9a1a6b955796595c87be1d
McLean Baran <baranmcl@gmail.com>
2015-07-10 00:20:29 -0400

    re-jiggered heap rebalancing
    removed need for heapq module

--- TwitterChallenge/src/median_unique.py
+++ TwitterChallenge/src/median_unique.py
@@ -1,7 +1,6 @@
 # calculates the median number of unique words per tweet
 #Running Median
 import os
-import heapq
 
 path1 = 'tweet_input'
 path2 = 'tweet_output'
@@ -9,28 +8,42 @@ InputFileName = "tweets.txt"
 
 MaxHeap = [] #every key <= to current median
 MinHeap = [] #every key >= to current median
-    
+
 runningmedian = [] #initialize list of running medians
 
-def RebalanceHeap():
-    if (len(MaxHeap) - len(MinHeap)) > 1:
-        heapq.heappush(MinHeap, max(MaxHeap))
-        MaxHeap.remove((max(MaxHeap)))
-        heapq.heapify(MaxHeap)
-    elif (len(MinHeap) - len(MaxHeap)) > 1:
-        heapq.heappush(MaxHeap, min(MinHeap))
-        MinHeap.remove((min(MinHeap)))
-        heapq.heapify(MinHeap)
+def swap_if_greater(pIndex, cIndex, heap):
+    if heap[pIndex] < heap[cIndex]:
+        heap[pIndex], heap[cIndex] = heap[cIndex], heap[pIndex]
+
+def sift(pIndex, unsortedLen, heap):
+    greaterIndex = lambda x, y: x if heap[x] > heap[y] else y
+    while pIndex*2+2 < unsortedLen:
+        LeftcIndex = pIndex*2+1
+        RightcIndex = pIndex*2+2
+
+        greater_child_index = greaterIndex(LeftcIndex, RightcIndex)
+        
+        swap_if_greater(pIndex, greater_child_index, heap)
+
+        pIndex = greater_child_index
+
+def Add_To_Min_Heap(x):
+    MinHeap = [x*-1] + MinHeap
+    sift(0, len(MinHeap), MinHeap)
+
+def Add_To_Max_Heap(x):
+    MaxHeap = [x] + MaxHeap
+    sift(0, len(MaxHeap), MaxHeap)
 
 def findmedian():
     if len(MaxHeap) + len(MinHeap) == 0:
-        return 0
+        return "empty"
     elif len(MinHeap) > len(MaxHeap):
-        return float(MinHeap[0])
+        return float(MinHeap[0]*-1)
     elif len(MaxHeap) > len(MinHeap):
-        return float(max(MaxHeap))
+        return float(MaxHeap[0])
     elif len(MinHeap) == len(MaxHeap):
-        return float((max(MaxHeap) + MinHeap[0])/ 2.0)
+        return float((MaxHeap[0] + (MinHeap[0]*-1)/ 2.00)
 
 def mainmedian():
     os.chdir("..")
@@ -39,24 +52,18 @@ def mainmedian():
     for line in readfile: #loop through each line/tweet in input file
         words = line.strip().split()
         currentmedian = findmedian()
-        if currentmedian != 0:
-            runningmedian.append(currentmedian)
-        if len(words) < currentmedian:
-            heapq.heappush(MaxHeap, len(words))
-            RebalanceHeap()
+        if currentmedian == "empty":
+            runningmedian.append(len(words))
+        elif len(words) < currentmedian:
+            Add_To_Max_Heap(len(words))
         elif len(words) > currentmedian:
-            heapq.heappush(MinHeap, len(words))
-            RebalanceHeap()
+            Add_To_Min_Heap(len(words))
         elif len(words) == currentmedian:
             if len(MaxHeap) > len(MinHeap):
-                heapq.heappush(MinHeap, len(words))
-                RebalanceHeap()
-            elif len(MaxHeap) < len(MinHeap):
-                heapq.heappush(MaxHeap, len(words))
-                RebalanceHeap()
-            elif len(MaxHeap) == len(MinHeap):
-                heapq.heappush(MaxHeap, len(words))
-                RebalanceHeap()
+                Add_To_Min_Heap(len(words))
+            else:
+                Add_To_Max_Heap(len(words))
+                     
     readfile.close()
     
     currentmedian = findmedian()
